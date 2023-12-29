59ea2568ff00c3405e383c5fc8663d127eaf8160
Arne Babenhauserheide <arne_bab@web.de>
2023-12-29 01:50:36 +0100

    Filter notifications, too

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -14,11 +14,53 @@ class Api::V1::NotificationsController < Api::BaseController
       @relationships = StatusRelationshipsPresenter.new(target_statuses_from_notifications, current_user&.account_id)
     end
 
-    render json: @notifications, each_serializer: REST::NotificationSerializer, relationships: @relationships
+    require 'pp'
+    require 'net/http'
+    puts "notifications index"
+    pp(@notifications)
+    ownkey_request = Net::HTTP.get_response(URI('http://127.0.0.1:4280/key/' + current_user&.account_id.to_s))
+    # if the own id does not exist yet, create it
+    pp(ownkey_request)
+    if not ownkey_request.is_a?(Net::HTTPSuccess)
+      Net::HTTP.post_form(URI('http://127.0.0.1:4280/trust/d6.gnutella2.info'), '100' => current_user&.account_id.to_s)
+      ownkey_request = Net::HTTP.get_response(URI('http://127.0.0.1:4280/key/' + current_user&.account_id.to_s))
+      puts "ownkey request"
+      pp(ownkey_request)
+    end
+    ownkey = ownkey_request.body
+    puts "ownkey"
+    pp(ownkey)
+
+    @notifications_filtered = @notifications.select do |notification|
+      # filter only status, polls, update, follow_request, favorite
+      puts 'notification.type'
+      pp(notification.type)
+      if notification.type.in?([:status, :polls, :update, :follow_request, :favorite])
+        pp(notification.type)
+        account_request = Net::HTTP.get_response(URI('http://127.0.0.1:4280/key/' + notification.from_account_id.to_s))
+        if account_request.is_a?(Net::HTTPSuccess)
+          account_key = account_request.body
+          puts "account_key"
+          pp(account_key)
+          result = Net::HTTP.get_response(URI('http://127.0.0.1:4280/score/ownkey/' + ownkey + '/otherkey/' + account_key))
+          puts "result"
+          pp(result)
+          puts "result.body"
+          pp(result.body)
+          (result.body && result.body.to_i >= 0) if result.is_a?(Net::HTTPSuccess)
+        else
+          false
+        end
+      else
+        true
+      end
+    end
+    render json: @notifications_filtered, each_serializer: REST::NotificationSerializer, relationships: @relationships
   end
 
   def show
     @notification = current_account.notifications.without_suspended.find(params[:id])
+    puts 'notification show'
     render json: @notification, serializer: REST::NotificationSerializer
   end
 
